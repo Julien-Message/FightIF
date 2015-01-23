c93fca6b442a3fa6364f2fcd92f8103efa6d066e
Galadruin <galadruin@gmail.com>
2015-01-23 01:22:28 +0100

    Revert "commit qui marche bis"
    This reverts commit 6730831413a2277485f21e36222daafab8cb1c52.

--- character.lua
+++ character.lua
@@ -202,6 +202,7 @@ end
 
 function Character:getCurrentPicture()
     currentState = self:getState()
+    print (" blah ", self:getState(), self.spritesFolders[self:getState()], self.currentPic, self.sprites[self.spritesFolders[self:getState()]])
     return self.sprites[self.spritesFolders[self:getState()]][self.currentPic]
 end
 
