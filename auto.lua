61bb9795619cbb9d76bc8b693562d8fe3fc69dec
Quentin Dunoyer <quentin.dunoyer@insa-lyon.fr>
2015-01-26 14:41:56 +0100

    Revert "EVEN MOAR STATES"
    This reverts commit f4c551567abd154e66de2aaf8d04a15fbc06f2c9.
    
    	modified:   auto.lua
    	modified:   character.lua

--- auto.lua
+++ auto.lua
@@ -9,35 +9,26 @@ end
 
 function Automate:applyEvent(event) --returns true if the state has changed
     local lastState = self.currentState
-    if event then
+    if event then -- an event has been 
         local newState = self.states[self.currentState][event]
-        if newState then -- a nil newState means the transition doesn't exist so we assume that we have to stay in the same state
-			if self.states[self.currentState]["timer"] then  -- Check if there is a minimum time to stay in that state.
-				self.nextState = self.states[self.currentState]["timer"]
-			else
-				self.currentState = newState
-			end
+        if newState then -- we check if there is a state corresponding to the event
+			self.currentState = newState
+            if self.states[self.currentState]["Timer"] then
+                self.lastTimer = love.timer.getTime()
+            end
+            return true
         end
     end
+    return false
+end
 
-    if lastState == self.currentState then
-        if self.states[self.currentState]["Timer"]
-            and (love.timer.getTime() - self.lastTimer) > self.states[self.currentState]["Timer"]
-			and self.nextState == currentState then
-            self.currentState = self.states[self.currentState]["Time"]
-            self.lastTimer = love.timer.getTime()
-        end
-		if self.states[self.currentState]["timer"]
-			and (love.timer.getTime() - self.lastTimer) > self.states[self.currentState]["Timer"]
-			and self.nextState ~= self.currentState then
-			self.currentState = self.nextState
-			self.lastTimer = love.timer.getTime()
-			self.nextState = self.currentState
-		end
-        return false
-    else
+function Automate:checkTimer()
+    if self.states[self.currentState]["Timer"]  -- we just check if a timer is finished
+        and (love.timer.getTime() - self.lastTimer) > self.states[self.currentState]["Timer"] then
+        self.currentState = self.states[self.currentState]["Time"]
         self.lastTimer = love.timer.getTime()
-		self.nextState = self.currentState
         return true
+    else
+        return false
     end
 end
