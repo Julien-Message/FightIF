1fbfabd9ba0d0085e7459aff03473f7d5ac66a29
Quentin Dunoyer <quentin.dunoyer@insa-lyon.fr>
2015-01-26 11:45:22 +0100

    Changement d'automate pour delayed state change

--- auto.lua
+++ auto.lua
@@ -3,6 +3,7 @@ Automate = class('Automate') -- class Automate
 function Automate:initialize(states, state) -- Initalisation Automate
     self.states = states
     self.currentState = state
+	self.nextState = state
     self.lastTimer = love.timer.getTime()
 end
 
@@ -11,7 +12,11 @@ function Automate:applyEvent(event) --returns true if the state has changed
     if event then
         local newState = self.states[self.currentState][event]
         if newState then -- a nil newState means the transition doesn't exist so we assume that we have to stay in the same state
-            self.currentState = newState
+			if self.states[self.currentState]["timer"] then  -- Check if there is a minimum time to stay in that state.
+				self.nextState = self.states[self.currentState]["timer"]
+			else
+				self.currentState = newState
+			end
         end
     end
 
@@ -21,6 +26,13 @@ function Automate:applyEvent(event) --returns true if the state has changed
             self.currentState = self.states[self.currentState]["Time"]
             self.lastTimer = love.timer.getTime()
         end
+		if self.states[self.currentState]["timer"]
+			and (love.timer.getTime() - self.lastTimer) > self.states[self.currentState]["Timer"]
+			and self.nextState ~= self.currentState then
+			self.currentState = self.nextState
+			self.lastTimer = love.timer.getTime()
+			self.nextState = self.currentState
+		end
         return false
     else
         self.lastTimer = love.timer.getTime()
