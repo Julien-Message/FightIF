303d9f47867c3bf5abd4e5e55e6bf435370b76f3
Julien Message <julien.message@mail.com>
2015-01-28 22:07:51 +0100

    BADASS

--- character.lua
+++ character.lua
@@ -5,41 +5,6 @@ local Character = class('Character')
 
 Character.static.width = 64
 Character.static.height = 128
-Character.static.punchLength = 32
-
-Character.static.states = {
-    Idle = {move = "Moving", jump = "Jumping", fall = "Falling", guard = "Guarding", punch = "Punching", kick = "Kicking", hit = "Idle", stunningHit = "Stunned"},
-    
-    Moving = {stop="Idle", move = "Moving", jump = "Jumping", fall = "Falling", punch = "PunchingForward", kick = "KickingForward", guard = "Guarding", hit = "Idle", stunningHit = "Stunned"},
-    
-    Jumping = {Time = "Falling", punch = "Uppercut", kick = "JumpingKick", MaxTime = 0.3},
-    Falling = {move="Falling", hitTheGround = "Idle", punch = "Uppercut", kick = "JumpingKick", hitTheGroundMoving = "Moving", jump = "JumpingAgain"},
-    FallingAfterPunch = {move = "FallingAfterPunch", hitTheGround = "Idle", hitTheGroundMoving = "Moving", jump = "JumpingAgain"},
-    
-    JumpingAgain = {Time = "FallingAgain", punch = "UppercutSecondJump", kick = "JumpingKick", MaxTime = 0.3},
-    FallingAgain = {move = "FallingAgain", hitTheGround = "Idle", punch = "UppercutSecondJump", kick = "JumpingKick", hitTheGroundMoving = "Moving"},
-    FallingAgainAfterPunch = {move = "FallingAgainAfterPunch", hitTheGround = "Idle", hitTheGroundMoving = "Moving"},
-    
-    Guarding = {stop = "Idle"},
-    
-    Punching = {punch = "Punching2", kick = "Kicking2", MinTime = 0.3, hit = "Idle", stunningHit = "Stunned", Time = "Idle", MaxTime = 0.4},
-    Punching2 = {punch = "PunchingFinal", kick = "KickingFinal", MinTime = 0.3, hit = "Idle", stunningHit = "Stunned", Time = "Idle", MaxTime = 0.4},
-    PunchingFinal = {hit = "Idle", stunningHit = "Stunned", Time = "Idle", MaxTime = 0.5},
-    
-    Kicking = {kick = "Kicking2", punch = "Punching2", MinTime = 0.5, hit = "Idle", stunningHit = "Stunned", Time = "Idle", MaxTime = 0.5},
-    Kicking2 = {kick = "KickingFinal", punch = "PunchingFinal", MinTime = 0.5, hit = "Idle", stunningHit = "Stunned", Time = "Idle", MaxTime = 0.5},
-    KickingFinal = {hit = "Idle", stunningHit = "Stunned", Time = "Idle", MaxTime = 0.7},
-    
-    PunchingForward = {Time = "Moving", hit = "Idle", stunningHit = "Stunned", MaxTime = 0.3},
-    KickingForward = {Time = "Moving", hit = "Idle", stunningHit = "Stunned", MaxTime = 0.3},
-    
-    Uppercut = {Time = "FallingAfterPunch", MaxTime = 0.5, hitTheGround = "Idle", hitTheGroundMoving = "Moving"},
-    UppercutSecondJump = {Time = "FallingAgainAfterPunch", MaxTime = 0.5, hitTheGround = "Idle", hitTheGroundMoving = "Moving"},
-    
-    JumpingKick = {hitTheGround = "Idle", hitTheGroundMoving = "Moving", Time = "Falling", MaxTime = 1, hitTheGround = "Idle", hitTheGroundMoving = "Moving"},
-    
-    Stunned = {Time = "Idle", MaxTime = 0.75}
-}
 
 Character.static.spritesFolders = {
     Idle = "idle",
@@ -70,134 +35,6 @@ Character.static.spritesFolders = {
     Stunned = "stun"
 }
 
-Character.static.actions = {
-    right = function (character)
-        if character.automate:applyEvent("move") then
-            local _,y = character.body:getLinearVelocity()
-            character.body:setLinearVelocity(character.speed, y)
-            --character.facingRight = true
-            return true
-        else
-            return false
-        end
-    end,
-
-    left = function (character)
-        if character.automate:applyEvent("move") then
-            local _,y = character.body:getLinearVelocity()
-            character.body:setLinearVelocity(-character.speed, y)
-            --character.facingRight = false
-            return true
-        else
-            return false
-        end
-    end,
-
-    jump = function (character)
-        if character.automate:applyEvent("jump") then
-            local x,y = character.body:getLinearVelocity()
-            character.body:setLinearVelocity(x, -character.jumpSpeed)
-            return true
-        end
-    end,
-
-    guard = function (character)
-        if character.automate:applyEvent("guard") then
-            character.body:setLinearVelocity(0,0)
-            return true
-        else
-            return false
-        end
-    end,
-
-    punch = function (character)
-        print("before : "..character:getState())
-        if character.automate:applyEvent("punch") then
-            print("after : "..character:getState())
-            for _,opponent in ipairs(characters) do
-                if opponent ~= character then
-                    if love.physics.getDistance(character.fixture, opponent.fixture) < Character.punchLength then
-                        if character:getState() == "PunchingFinal" then
-                            opponent:applyAction("takeAStunningHit")
-                        else
-                            opponent:applyAction("takeAHit")
-                        end
-                    end
-                end
-            end
-
-            return true
-        else
-            return false
-        end
-    end,
-
-    kick = function (character)
-        if character.automate:applyEvent("kick") then
-            return true
-        else
-            return false
-        end
-    end,
-
-    takeAHit = function (character)
-        if character.automate:applyEvent("hit") then
-            return true
-        else
-            return false
-        end
-    end,
-
-    takeAStunningHit = function (character)
-        if character.automate:applyEvent("stunningHit") then
-            return true
-        else
-            return false
-        end
-    end,
-
-    stop = function (character)
-        if character.automate:applyEvent("stop") then
-            --character.body:setLinearVelocity(0,0)
-            return true
-        else
-            return false
-        end
-    end,
-
-    hitTheGround = function (character)
-        local dx,_ = character.body:getLinearVelocity()
-        if dx == 0 then
-            return character.automate:applyEvent("hitTheGround")
-        else
-            return character.automate:applyEvent("hitTheGroundMoving")
-        end
-    end,
-
-    noInput = function (character)
-        if character:getState() == "Moving" or character:getState() == "Guarding" then
-            return character:applyAction("stop")
-        end
-    end,
-
-    default = function (character)
-        local x, y, dx, dy = character.body:getX(), character.body:getY(), character.body:getLinearVelocity()
-        local state = character:getState()
-        local result = false
-
-        if Character.states[state]["fall"] and dy > 10 then
-           result = result or character.automate:applyEvent("fall")
-        end
-
-        if state == "Idle" or state == "Stunned" or state == Character.states[state]["hitTheGround"] then
-            character.body:setLinearVelocity(dx * 0.8, dy)
-        end
-        --check if a timer has finished
-        result = result or character.automate:checkTimer()
-        return result
-    end
-}
-
 function Character:initialize(name, x, y, _controller)
     self.name = name
 
@@ -216,7 +53,7 @@ function Character:initialize(name, x, y, _controller)
     self.speed = 200
     self.jumpSpeed = 500
 
-    self.automate = Automate(Character.states, "Idle")
+    self.automate = Automate(self)
     self.lastState = "Idle"
 
     -- loads the sprites into a table for quick access
@@ -244,7 +81,7 @@ end
 --looks which key is down and do the necessary things
 function Character:update(dt)
     self.controller:update()
-    self:applyAction("default")
+    Automate.actions["default"](self)
     
     local actionDone = false
     for _,action in ipairs(self.continuousActions) do
@@ -268,7 +105,7 @@ end
 
 -- applies the action on the character
 function Character:applyAction(action)
-    if Character.actions[action](self) and self:getState() ~= self.lastState then --if the state has changed
+    if self.automate:applyAction(action) and self:getState() ~= self.lastState then --if the state has changed
         self.lastState = self:getState()
         self:loadAnimation()
     end
@@ -276,31 +113,6 @@ end
 
 function Character:getState()
     return self.automate.currentState
-end
-
-
-function Character:getNextState()
-    return self.automate.nextState
-end
-
-function Character:canMove()
-    local currentState = self:getState()
-    for _,state in ipairs(Character.movingStates) do
-        if currentState == state then
-            return true
-        end
-    end
-    return false
-end
-
-function Character:canJump()
-    local currentState = self:getState()
-    for _,state in ipairs(Character.jumpingStates) do
-        if currentState == state then
-            return true
-        end
-    end
-    return false
 end
 
 function Character:loadAnimation()
@@ -342,8 +154,7 @@ function Character:drawDebug()
         "\nPicture Number : " .. self.currentPic ..
         "\nState is : " .. self:getState() ..
         "\nMass is : " .. self.body:getMass()..
-        "\nAutoTimer = " .. self.automate.lastTimer ..
-        "\nnextState is :" .. self:getNextState(),
+        "\nAutoTimer = " .. self.automate.lastTimer,
         self.body:getX(), self.body:getY() - 150)
 end
 
